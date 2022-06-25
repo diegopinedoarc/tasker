92fad1ce98dbb4940b98a5c24d7eee272059aa65
Diego Pinedo <diegopinedo@outlook.com>
2022-06-25 19:47:23 -0300

    act

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let inputText = document.querySelector('#inputText');
 // -- Traigo el boton para agregar tareas --
 let addTaskBtn = document.getElementById('addTask');
 // -- Traigo la lista donde van colocadas las tareas --
-let listContainer = document.getElementsByClassName('list_container');
+let listContainer = document.getElementById('list_container');
 // -- Compruebo estar trayendo bien los elementos --
 // console.log(inputText);
 // console.log(addTask);
@@ -15,7 +15,7 @@ addTaskBtn.addEventListener('click', addTasks);
 
 function addTasks() {
     // -- Reinicio el valor del input, para que este vacio --
-    const task = input.value;
+    const task = inputText.value;
     // -- Con if le damos 2 opciones al input: --
     if (task === '') {
         showError('La tarea esta vacia');
@@ -27,16 +27,30 @@ function addTasks() {
     };
     // -- Reasigno el contenido del objeto al array vacio --
     tasks = [...tasks, taskObj];
+    console.log(tasks)
     // -- Ejecutamos la funcion que crea el li 
     createHTML();
-    input.value = "";
+    inputText.value = "";
 
 }
 
 function createHTML() {
     listContainer.innerHTML = "";
     tasks.forEach((task) => {
-        
+        const li = document.createElement('li');
+        li.innerHTML = `${task.task}          <span data-id='${task.id}'><i class="fa-solid fa-square-check"></i></span></li>`;
+        listContainer.appendChild(li);
     })
+}
+
+let showError = (error) => {
+    const msgError = document.createElement('p');
+    msgError.textContent = error;
+    msgError.classList.add('error');
+    listContainer.appendChild(msgError);
+    settimeout(()=> {
+        msgError.remove();
+    },2000)
+
 }
 
